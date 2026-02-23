4ed05e4174ea7237589cb732989904416a4baf1c
kimgang91 <kimgang91@gmail.com>
2026-02-23 19:46:13 +0900

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
+
